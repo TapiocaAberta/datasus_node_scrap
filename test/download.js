7587fc5b9337209e0fc67634d71b7cc0255a9fc1
Paulo Luan <pauloluan.inova@gmail.com>
2015-07-28 14:02:57 -0300

    added tests

--- test/download.js
+++ test/download.js
@@ -1,12 +1,15 @@
-var module = require('../index'),
+var module = require('../crawler/download'),
     path = require('path'),
     _ = require('lodash'),
     fs = require('fs'),
     should = require('should');
+var baseUrl = 'http://cnes.datasus.gov.br/';
 describe('#init', function() {
     this.timeout(5000);
-    it('should execute the main function in order to execute the complete cycle of the app.', function(done) {
-        return module.init().then(function(result) {
+    it('should download the base page.', function(done) {
+        return module.doGet(baseUrl).then(function(result) {
+            console.log(result);
+            result.should.be.ok;
             done();
         }).catch(function(error) {
             console.log(error);
