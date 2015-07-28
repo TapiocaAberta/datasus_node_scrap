cff404491aee3b91b8e83854aa065f054fea6f18
Paulo Luan <pauloluan.inova@gmail.com>
2015-07-28 11:42:27 -0300

    added hint scripts

--- script/formatCode.sh
+++ script/formatCode.sh
@@ -2,12 +2,10 @@
 # you must install https://www.npmjs.com/package/js-beautify
 # npm i -g js-beautify
 
-set -e
-
 find -regex ".*\.\(js\|json\|css\|html\)" \
     -not -path "./bower_components/*" \
     -not -path "./node_modules/*" \
     -not -path "./dist/*" \
     -not -path "./.publish/*" \
     -not -path "./.tmp/*" \
-    | xargs js-beautify --quiet --replace
+    | xargs js-beautify --replace
